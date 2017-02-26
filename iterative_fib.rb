185d508742561b0aad0d1001958118cfa9e25b8b
erikyoung <erik.young@rmit.edu.vn>
2017-02-26 23:15:29 +0700

    Create iterative_fib.rb

---
+++ iterative_fib.rb
@@ -0,0 +1,25 @@
+def fibonacci(n)
+  n1 = 1
+  n2 = 2
+  n3 = n1 + n2
+
+  n.times do 
+    n3 = n1
+    n1 = n2
+    n2 = n3 + n1
+  end
+  return n1  
+end
+
+8.times do |n|
+    result = fibonacci(n)
+    puts result
+end
+  
+#0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+  
+    
+
+    
+  
